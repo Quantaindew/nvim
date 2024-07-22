1d2248295102191ed1fb164dd2993b204acfc61e
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-07-22 21:21:40 +0530

    disabling method unsucessful

--- init.lua
+++ init.lua
@@ -44,17 +44,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     print("LSP started:", client.name)
   end,
 })
-
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  callback = function()
-    vim.diagnostic.disable(0)
-    local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-    for _, client in ipairs(clients) do
-      if client.name == "lua_ls" then
-        vim.lsp.stop_client(client.id)
-      end
-    end
-  end,
-})
