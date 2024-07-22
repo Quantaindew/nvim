9918ae5cd9edd83cd53efc0d39d17262adaa4e0d
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-07-22 20:56:41 +0530

    remove lua server auto-loading

--- lua/configs/mason_lspconfig.lua
+++ lua/configs/mason_lspconfig.lua
@@ -1,12 +1,11 @@
 -- ~/.config/nvim/lua/configs/mason_lspconfig.lua
-
 local M = {}
 
 M.setup = function()
   require("mason-lspconfig").setup {
     ensure_installed = {
-      --  "lua_ls",
-      --  "rust_analyzer"
+      -- "lua_ls" is removed from here
+      -- Add other language servers you want to ensure are installed
     },
     automatic_installation = true,
   }
@@ -16,11 +15,14 @@ M.setup = function()
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
     function(server_name) -- default handler (optional)
-      require("lspconfig")[server_name].setup {
-        on_attach = require("nvchad.configs.lspconfig").on_attach,
-        on_init = require("nvchad.configs.lspconfig").on_init,
-        capabilities = require("nvchad.configs.lspconfig").capabilities,
-      }
+      -- Skip setup for lua_ls
+      if server_name ~= "lua_ls" then
+        require("lspconfig")[server_name].setup {
+          on_attach = require("nvchad.configs.lspconfig").on_attach,
+          on_init = require("nvchad.configs.lspconfig").on_init,
+          capabilities = require("nvchad.configs.lspconfig").capabilities,
+        }
+      end
     end,
   }
 end
