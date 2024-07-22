2a82d53d6e6f3f027b285d6619a8cf6b21c151f0
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-07-22 21:35:50 +0530

    lsp disabling sucessful

--- lua/configs/mason_lspconfig.lua
+++ lua/configs/mason_lspconfig.lua
@@ -3,30 +3,27 @@ local M = {}
 
 M.setup = function()
   require("mason-lspconfig").setup {
-    ensure_installed = {
-      -- "lua_ls" is removed from here
-      -- Add other language servers you want to ensure are installed
-    },
+    ensure_installed = {},
     automatic_installation = true,
   }
 
-  require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- default handler (optional)
-      -- Skip setup for lua_ls
-      if server_name ~= "lua_ls" then
-        require("lspconfig")[server_name].setup {
-          on_attach = require("nvchad.configs.lspconfig").on_attach,
-          on_init = require("nvchad.configs.lspconfig").on_init,
-          capabilities = require("nvchad.configs.lspconfig").capabilities,
-        }
-      end
-    end,
-  }
-  -- Explicitly disable lua_ls
-  require("lspconfig").lua_ls.setup = function() end
+  local lspconfig = require("lspconfig")
+  local get_servers = require("mason-lspconfig").get_installed_servers
+
+  local skip_servers = { "lua_ls" }
+
+  for _, server_name in ipairs(get_servers()) do
+    if not vim.tbl_contains(skip_servers, server_name) then
+      lspconfig[server_name].setup {
+        on_attach = require("nvchad.configs.lspconfig").on_attach,
+        on_init = require("nvchad.configs.lspconfig").on_init,
+        capabilities = require("nvchad.configs.lspconfig").capabilities,
+      }
+    end
+  end
+
+  -- Explicitly prevent lua_ls from being set up
+  lspconfig.lua_ls.setup = function() end
 end
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,10 +37,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    --  config = function()
-    --      require("nvchad.configs.lspconfig").defaults()
-    --      require "configs.lspconfig"
-    --      end,
+    config = function()
+      --      require("nvchad.configs.lspconfig").defaults()
+      --      require "configs.lspconfig"
+    end,
   },
 
   {
