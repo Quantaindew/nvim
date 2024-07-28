e8f7730e71170b6cc61c8cac730ef4d415a1776a
Sanjay S Kumar <35188255+Quantaindew@users.noreply.github.com>
2024-07-28 20:02:09 +0530

    Create mason.lua

---
+++ lua/configs/mason.lua
@@ -0,0 +1,38 @@
+local config = {
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+
+  ensure_installed = {
+    -- LSP
+    -- "lua-language-server",
+    "html-lsp",
+    "css-lsp",
+    "typescript-language-server",
+    "python-lsp-server",
+    -- "rust-analyzer",
+    "zls",
+
+    -- Linters
+    -- "eslint_d",
+    -- "flake8",
+
+    -- Formatters
+    "stylua",
+    "prettier",
+    "black",
+    ---"rustfmt",
+
+
+
+
+  },
+
+  max_concurrent_installers = 10,
+}
+
+return config
